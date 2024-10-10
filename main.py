c29d9f791769857e44e8d5e17fe830e9d226a757
sanagn <150658259+sanagn@users.noreply.github.com>
2024-10-10 13:05:02 +0300

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+'''
+Robot follow line with HuskyLens camera, Raspbery Pi Pico and ELECFREAKS Wukong2040 Breakout Board
+Simos Anagnostakis sanagn@uoc.gr
+2023
+'''
 import machine
 from huskylensPythonLibrary import HuskyLensLibrary
 import utime
@@ -5,8 +10,10 @@ import time
 import uos
 
 husky = HuskyLensLibrary("I2C") ## SERIAL OR I2C
-#φροντιζουμε η καμερα να είναι σε κατασταση  akoloyuhgse th grammh
+# we make sure the camera is in Follow Line mode
+# φροντίζουμε η καμερα να είναι σε κατασταση Follow Line
 
+# We define the GPIO pins for the robot's motors
 # Ορίζουμε τους ακροδέκτες GPIO για τους κινητήρες του ρομπότ
 M1A = machine.PWM(machine.Pin(10)) 
 M1B = machine.PWM(machine.Pin(11)) 
@@ -18,24 +25,27 @@ M2A.freq(50)
 M2B.freq(50)
 
 # Ορίζουμε τη λειτουργία των κινητήρων του ρομπότ
-def move__forword(speed): # Ο κύκλος λειτουργίας πρέπει να είναι μεταξύ 0 και 65535
+# We define the operation of the robot's motors
+# Ο κύκλος λειτουργίας πρέπει να είναι μεταξύ 0 και 65535
+# The duty cycle must be between 0 and 65535
+def move__forword(speed): 
     #print("Fspeed"+str(speed))
     M1A.duty_u16(0) 
     M1B.duty_u16(speed)
     M2A.duty_u16(0)
     M2B.duty_u16(speed)  
-def move__backword(speed): # Ο κύκλος λειτουργίας πρέπει να είναι μεταξύ 0 και 65535
+def move__backword(speed): 
     M1A.duty_u16(speed)
     M1B.duty_u16(0)
     M2A.duty_u16(speed)
     M2B.duty_u16(0)      
-def move_left(speed): # Ο κύκλος λειτουργίας πρέπει να είναι μεταξύ 0 και 65535
+def move_left(speed):
     #print("Lspeed"+str(speed))
     M1A.duty_u16(0) 
     M1B.duty_u16(0)
     M2A.duty_u16(0)
     M2B.duty_u16(speed)
-def move_right(speed):# Ο κύκλος λειτουργίας πρέπει να είναι μεταξύ 0 και 65535
+def move_right(speed):
     #print("Rspeed"+str(speed))
     M1A.duty_u16(0) 
     M1B.duty_u16(speed)
@@ -62,16 +72,17 @@ while True:
         YTail= i[1]
         XHead = int(i[2])
         YHead  = i[3]
-        #print ("Χ-αρχής="+ str(XTail) +", Υ-αρχής="+ str(YTail)+",Χ-τελους="+str(XHead)+", Υ-τέλους="+str(YHead))
+        #print ("Χ-start="+ str(XTail) +", Υ-start="+ str(YTail)+",Χ-end="+str(XHead)+", Υ-endς="+str(YHead))
     Rten = XHead - 160 #160 είναι το μέσο Χ της κάμερας
     Rdir = XHead
     #print ("Rten="+str(Rten)+", Rdir="+ str(Rdir)) 
     #error = (int32_t)result.xTarget;
     # Analyze data and determine the position of the line
+    # Αναλύστε δεδομένα και προσδιορίστε τη θέση της γραμμής
     # Example: Parse data to extract line position information
-
+    # Παράδειγμα: Ανάλυση δεδομένων για εξαγωγή πληροφοριών θέσης γραμμής
     # Control motors based on line position
-    # Example: If line is more to the left, turn left. If more to the right, turn right.
+    # Example: If line is more to the left, turn rigj. If more to the right, turn left.
     if Rdir > 180:
         move_right(abs(Rten)*400)
         utime.sleep_ms(abs(Rten))
@@ -84,10 +95,6 @@ while True:
         stop_robot(0)
     utime.sleep_ms(10)  # Adjust as needed for control frequency
     
-
-# Ορίζουμε τις λειτουργίες των κινητήρων για να κινούν το ρομπότ μπροστά
-
-# Ορίζουμε τις λειτουργίες των κινητήρων για να σταματήσουν το ρομπότ
 
 
 
